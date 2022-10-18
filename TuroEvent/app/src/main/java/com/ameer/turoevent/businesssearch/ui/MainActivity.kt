1ba2adf9b9a1cf9e09b44a5f9aa3e291c3a1ecdb
Ameer Hashim <1318067+ameerirsh@users.noreply.github.com>
2022-10-18 08:33:47 -0700

    Repo changes
    Repo changes

--- TuroEvent/app/src/main/java/com/ameer/turoevent/businesssearch/ui/MainActivity.kt
+++ TuroEvent/app/src/main/java/com/ameer/turoevent/businesssearch/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
--- TuroEvent/app/src/main/java/com/ameer/turoevent/businesssearch/ui/MainViewModel.kt
+++ TuroEvent/app/src/main/java/com/ameer/turoevent/businesssearch/ui/MainViewModel.kt
@@ -1,10 +1,14 @@
 package com.ameer.turoevent.businesssearch
 
+import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.ameer.turoevent.AppClass
 import com.ameer.turoevent.analytics.AnalyticsTracker
 import com.ameer.turoevent.analytics.EventConstants.Companion.EVENT_API_SEARCH
 import com.ameer.turoevent.analytics.EventConstants.Companion.EVENT_NETWORK_ERROR
@@ -13,6 +17,7 @@ import com.example.example.Businesses
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,13 +28,26 @@ private const val SEARCH_KEY_PIZZA = "pizza"
 private const val SEARCH_KEY_BEER = "beer"
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val apiService: ApiService) : ViewModel() {
+class MainViewModel @Inject constructor(var apiService: ApiService) : ViewModel() {
+    //private val businessSearchRepository = getApplication<AppClass>().businessSearchRepository
     var analyticsTracker: AnalyticsTracker = AnalyticsTracker()
     var errorMessage: String by mutableStateOf("")
     var busListResponse: List<Businesses> by mutableStateOf(listOf())
     var pizzaList: List<Businesses> by mutableStateOf(listOf())
     var beerList: List<Businesses> by mutableStateOf(listOf())
     val businessListState = MutableStateFlow<BusinessListState>(BusinessListState.START)
+
+    private val allBusiness = MutableStateFlow(emptyFlow<PagingData<Businesses>>())
+
+    /*fun getPizzaAndBeerListNew() {
+        viewModelScope.launch {
+            try {
+                allBusiness.value = businessSearchRepository.searchResults()
+            } catch (e: Exception) {
+
+            }
+        }
+    }*/
 
     fun getPizzaAndBeerList() {
             viewModelScope.launch {
